de08be220f7610ff870678aed4726c7bc25388b9
furikus <danikrogov@bk.ru>
2024-04-03 16:30:33 +0300

    labs

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,12 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.lang.Math;
 public class Main {
-    public static <string> void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-
-        int leng = String.valueOf(num).length();
-        int reverse = 0;
-
-        for (int i = 0; i < leng; i++){
-            int remainder = num % 10;
-            reverse = reverse * 10 + remainder;
-            num = num/10;
-        }
-
-
-        System.out.println(reverse);
+        String text;
+        text = scanner.toString();
+        System.out.println(text);
     }
-
 
 
 }
